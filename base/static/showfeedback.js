3b345231e18f9761954a5e08ddc98c660e766bff
Carsten Knoll <carsten.knoll@posteo.de>
2024-04-06 14:36:59 +0200

    showfeedback.js: next step towards display of annotations from database

--- base/static/showfeedback.js
+++ base/static/showfeedback.js
@@ -1,24 +1,47 @@
+// import Highlighter from './Highlighter.js';
 const csrftoken = Cookies.get('csrftoken');
 
+
 var sac = document.getElementById("show_annotations_content");
+var hl = new Highlighter(sac, null);
+var ann =  {
+    "@context":"http://www.w3.org/ns/anno.jsonld",
+    "type":"Annotation",
+    "body":[
+        {"type":"TextualBody","value":"comment1 like model","purpose":"commenting"}
+    ],
+    "target":
+    {
+        "selector":[
+            {"type":"TextQuoteSelector","exact":"document"},
+            {"type":"TextPositionSelector","start":32,"end":40}
+        ]
+    },
+    "id":"#95937184-078a-49c0-8037-d8495b593674",
+    "start":2,
+    "end":27
+};
 
-// do some asynchronous work (use `await a`)
-const a = (async () => {
-    const fixedAnnotation = (await fetch("http://localhost:8000/api/get").then((response) => response.json()))[1];
-    const range = document.createRange();
-    const selector = fixedAnnotation.target.selector.filter(a => a.type == 'TextPositionSelector')[0]
+hl.addOrUpdateAnnotation(ann);
 
 
-    // range.setStart(ac, selector.start);
-    // range.setEnd(ac, selector.end);
+// // do some asynchronous work (use `await a`)
+// const a = (async () => {
+//     const fixedAnnotation = (await fetch("http://localhost:8000/api/get").then((response) => response.json()))[1];
+//     const range = document.createRange();
+//     const selector = fixedAnnotation.target.selector.filter(a => a.type == 'TextPositionSelector')[0]
 
-    // nontrivial problem: find the correct start-nodes + offset
-    range.setStart(sac, 1);
-    range.setEnd(sac, 3);
 
-    var span_wrapper = document.createElement('SPAN');
-    range.surroundContents(span_wrapper);
-    span_wrapper.className = "r6o-annotation";
+//     // range.setStart(ac, selector.start);
+//     // range.setEnd(ac, selector.end);
 
-    return fixedAnnotation
-  })()
+//     // nontrivial problem: find the correct start-nodes + offset
+//     range.setStart(sac, 1);
+//     range.setEnd(sac, 3);
+
+//     var span_wrapper = document.createElement('SPAN');
+//     range.surroundContents(span_wrapper);
+//     span_wrapper.className = "r6o-annotation";
+
+//     return fixedAnnotation
+//   })()
